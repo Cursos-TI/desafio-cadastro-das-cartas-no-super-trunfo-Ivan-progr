f1258cef695d2d258d86f89e6fae1909e630ff8e
Ivan Ricardo Gonçalves Ferreira <ivanricardo.g.ferreira@gmail.com>
2025-08-07 22:39:53 +0000

    separando as cartas

---
+++ supertrunfosCartas.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+// desafio super trunfo, 2 cartas iniciais, dia 07/08
+int main() {
+// carta 1 
+    char estado = "B";
+    char codigo[20] = "B01";
+    char cidade[50] = "salvador";
+    int população = 2418005;
+    float km = 693.8;
+    float pib = 62.9;
+    int turisticos = 20; 
+
+// carta 2
+
+    char estado = "A";
+    char codigo[20] = "A01";
+    char cidade[50] = "Curitiba";
+    int  população = 1964787;
+    int km = 432; 
+    int pib = 98;
+    int turismo = 30;
+
+
+}
+
+
+
+
+
+
+
+
+
