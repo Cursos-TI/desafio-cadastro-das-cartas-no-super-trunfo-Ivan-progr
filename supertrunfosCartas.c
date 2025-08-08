4b9b7ae62206e7a381fe13a9f3167560ef36de47
Ivan Ricardo Gonçalves Ferreira <ivanricardo.g.ferreira@gmail.com>
2025-08-07 21:27:15 -0300

    Update supertrunfosCartas.c

--- supertrunfosCartas.c
+++ supertrunfosCartas.c
@@ -3,23 +3,23 @@
 // desafio super trunfo, 2 cartas iniciais, dia 07/08
 int main() {
 // carta 1 
-    char estado = "B";
-    char codigo[20] = "B01";
-    char cidade[50] = "salvador";
-    int população = 2418005;
-    float km = 693.8;
-    float pib = 62.9;
-    int turisticos = 20; 
+    char estado1 = "B";
+    char codigo1[20] = "B01";
+    char cidade1[50] = "salvador";
+    int população1 = 2418005;
+    float km1 = 693.8;
+    float pib1 = 62.9;
+    int turisticos1 = 20; 
 
 // carta 2
 
-    char estado = "A";
-    char codigo[20] = "A01";
-    char cidade[50] = "Curitiba";
-    int  população = 1964787;
-    int km = 432; 
-    int pib = 98;
-    int turismo = 30;
+    char estado2 = "A";
+    char codigo2[20] = "A01";
+    char cidade2[50] = "Curitiba";
+    int  população2 = 1964787;
+    int km2 = 432; 
+    int pib2 = 98;
+    int turismo2 = 30;
 
 // Entrada de dados da carta 1
     printf("=== Carta 1 ===\n");
